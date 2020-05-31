e87a5c6454617251d890013ce3ab62e5f3ad3fdd
han <1182036617@qq.com>
2020-05-31 13:45:03 +0800

    hanjinhui

---
+++ src/views/About.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <div>
+      <ul>
+        <li v-for="(item,i) in list" :key="i">{{item.name}}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import jsonp from "jsonp";
+export default {
+  data() {
+    return {
+      list: []
+    };
+  },
+  methods: {
+    getData() {
+      jsonp(
+        "https://interface.sina.cn/news/wap/fymap2020_data.d.json?_=1580892522427",
+        (err, res) => {
+          console.log(res.data.list);
+          this.list = res.data.list;
+        }
+      );
+    }
+  },
+  mounted() {
+    this.getData();
+  }
+};
+</script>
+
+<style scoped>
+li {
+  list-style: none;
+}
+</style>
