812c72c9e2693e772a9e162d7200360328ed2de6
han <1182036617@qq.com>
2020-06-02 15:35:20 +0800

    modify

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,30 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome！ 韩金辉 "/>
+    <HelloWorld :msg='this.msg' />
+    <el-button type="primary" @click="modify" :disabled='this.flag'>点击修改</el-button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
+  data() {
+    return {
+      msg: "Welcome！ 韩金辉 ",
+      flag:false
+    };
+  },
   components: {
     HelloWorld
+  },
+  methods: {
+    modify(){
+      this.msg = '少年，你好！！！',
+      this.flag = true
+    }
   }
-}
+};
 </script>
